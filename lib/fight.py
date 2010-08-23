28c4e2dcc0ef0ce08885fc0c7e8cc696b9e40df4
Patrick <saluk64007@gmail.com>
2010-08-22 17:45:46 -0700

    Can move around points on the map

--- lib/fight.py
+++ lib/fight.py
@@ -4,6 +4,7 @@ from things import *
 class spot(thing):
     """A spot someone can be placed in on the fight screen"""
     def __init__(self,pos,next=[]):
+        self.children = []
         self.pos = pos
         self.next = next
         self.contains = None
@@ -13,9 +14,16 @@ class spot(thing):
                 if n.enemy == True:
                     return n
     def can_move(self):
+        spots = []
         for n in self.next:
             if not n.contains:
-                return True
+                spots.append(n)
+        return spots
+    def update(self,dt):
+        if not self.children and self.contains:
+            self.children = [self.contains]
+        if not self.contains and self.children:
+            self.children = []
                 
 class realchar(thing):
     def __init__(self):
@@ -26,6 +34,8 @@ class realchar(thing):
         self.children = []
         self.enemy = False
     def set_spot(self,spot):
+        if self.spot:
+            self.spot.contains = None
         self.children = []
         if not spot.contains:
             spot.contains = self
@@ -64,11 +74,35 @@ class action_menu(menu):
         if character.spot.can_move():
             self.options.append("move")
         self.options.append("idle")
+    def move(self):
+        pygame.scene.children[0].menus.children = [move_menu(self.character)]
+
+class move_menu(thing):
+    def __init__(self,char):
+        self.children = []
+        self.char = char
+        self.start = self.char.spot
+        self.spots = self.char.spot.can_move()
+    def draw(self,surf):
+        for s in self.spots:
+            x = s.pos[0]-10
+            y = s.pos[1]-10
+            pygame.draw.rect(surf,[255,255,255],[[x,y],[20,20]])
+            pygame.draw.line(surf,[255,255,255],self.start.pos,s.pos)
+    def mouse_click(self,pos,mode):
+        x,y = pos
+        for s in self.spots:
+            cx = s.pos[0]-10
+            cy = s.pos[1]-10
+            cw=ch=20
+            if x>=cx and x<=cx+cw and y>=cy and y<=cy+ch:
+                self.char.set_spot(s)
+                pygame.scene.children[0].menus.children.remove(self)
+                return True
         
 class fight_scene(thing):
     def __init__(self):
         self.children = []
-        self.am = None
         self.bg = sprite("art/bunker.png",[0,0])
         self.children.append(self.bg)
         self.spots = {}
@@ -79,21 +113,21 @@ class fight_scene(thing):
         self.spots["left"] = spot([131,82])
         
         self.spots["door"].next = [self.spots["mid"]]
-        self.spots["mid"].next = [self.spots["left"],self.spots["right"]]
+        self.spots["mid"].next = [self.spots["left"],self.spots["right"],self.spots["door"]]
         self.spots["left"].next = [self.spots["mid"]]
         self.spots["right"].next = [self.spots["mid"]]
+        for s in self.spots.values():
+            self.children.append(s)
         
         enemy = realchar().set_spot(self.spots["door"])
         enemy.weapon = weapon()
         enemy.enemy = True
         player = realchar().set_spot(self.spots["mid"])
         player.weapon = weapon()
-    def update(self,dt):
-        for s in self.spots.values():
-            if s.contains and s.contains not in self.children:
-                self.children.append(s.contains)
+        player.sprite.set_facing("n")
+        
+        self.menus = thing()
+        self.children.append(self.menus)
     def action_menu(self,char):
-        if self.am in self.children:
-            self.children.remove(self.am)
-        self.am = action_menu(char)
-        self.children.append(self.am)
+        am = action_menu(char)
+        self.menus.children = [am]
